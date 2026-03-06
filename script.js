f42026375533e9fdc058823ae55e2ac3e4b45d39
alex1pg <alejandroiacobucci20@gmail.com>
2026-03-06 17:59:35 +0100

    fatto tante cose nel js

--- script.js
+++ script.js
@@ -12,11 +12,36 @@ const todolist = [
 ];
 
 const listaToDo = document.getElementById("lista-todo");
+const btnAdd = document.getElementById("btnAdd");
+const inputTodo = document.getElementById("label_in");
 
-todolist.forEach(
-    function(item) {
-        const elementoLi = document.createElement('li');
-        elementoLi.innerText= item.etichetta;
-        listaToDo.append(elementoLi);
+disegnaElenco();
+
+function disegnaElenco(){
+    listaToDo.innerText = '';
+
+    todolist.forEach(
+        function(item) {
+            const elementoLi = document.createElement('li');
+            elementoLi.innerText= item.etichetta;
+            elementoLi.classList.add("todoitem");
+            listaToDo.append(elementoLi);
+        }
+    ); 
+}
+ 
+
+btnAdd.addEventListener("click", addItem);
+
+function addItem(){
+
+    const newtodo = {
+        etichetta : inputTodo.value,
+        stato : false,
+        order: 1,
     }
-);  
+
+    todolist.push(newtodo);
+
+    disegnaElenco();
+}
