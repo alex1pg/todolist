7b1cc850fceac3e7df403ea9b947ad17db80b7f1
alex1pg <alejandroiacobucci20@gmail.com>
2026-03-06 17:13:40 +0100

    aggiunto l'array todolist

---
+++ script.js
@@ -0,0 +1,12 @@
+const todolist = [
+    {
+        etichetta: "prima todo",
+        stato: false,
+        order: 1,
+    },
+    {
+        etichetta: "seconda todo",
+        stato: false,
+        order: 1,
+    }
+];
